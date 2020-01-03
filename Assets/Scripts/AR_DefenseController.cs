0b6772f5ab81f8fd7ef06fa7e49aacce993122d7
Leonid <leonidb93@gmail.com>
2020-01-03 12:37:41 +0100

    the famous one commit project

---
+++ Assets/Scripts/AR_DefenseController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AR_DefenseController : MonoBehaviour
+{
+    public GameObject Cross;
+    public GameObject Arrow;
+    public GameObject WinText;
+    public AudioSource AS;
+    public AudioClip loopClip;
+
+    private bool startedLoopSound;
+
+    private PlacementController PC;
+    private bool gameStarted;
+    private float speed = 20;
+    private Transform mainCam;
+
+    private float projectileRespawnTime;
+
+    private void Start() {
+        mainCam = Camera.main.transform;
+        PC = GetComponent<PlacementController>();
+        Cross = GameObject.Find("Cross");
+        Cross.SetActive(false);
+        WinText.SetActive(false);
+
+
+    }
+
+    public void StartGame() {
+        PC.StartButton.SetActive(false);
+        PC.RemovePlanes();
+        PC.gameStarted = true;
+        Cross.SetActive(true);
+        AS.Play();
+        gameStarted = true;
+    }
+
+    private void Update() {
+        if (gameStarted) {
+            foreach (Transform enemy in PC.allPlacedObjects) {
+                enemy.position = Vector3.MoveTowards(enemy.position, mainCam.position, Time.deltaTime / speed);
+                enemy.LookAt(mainCam);
+            }
+
+            projectileRespawnTime += Time.deltaTime;
+
+            if (projectileRespawnTime >= 2) {
+                Transform projectile = Instantiate(Arrow, mainCam.position, mainCam.rotation).transform;
+
+                //projectile.rotation = mainCam.rotation;
+                projectile.localScale /= 10;
+                projectile.gameObject.AddComponent<ProjectileController>();
+                Rigidbody rg = projectile.gameObject.AddComponent<Rigidbody>();
+                rg.useGravity = false;
+                rg.AddForce(mainCam.forward * 50);
+
+                projectileRespawnTime = 0;
+            }
+
+            if (!AS.isPlaying && !startedLoopSound) {
+                AS.clip = loopClip;
+                AS.loop = true;
+                AS.Play();
+                startedLoopSound = true;
+            }
+
+        }
+    }
+
+    public void win() {
+        AS.Stop();
+        Cross.SetActive(false);
+        WinText.SetActive(true);
+        gameStarted = false;
+    }
+
+
+}
+
+
+public class ProjectileController : MonoBehaviour
+{
+    private float t;
+
+    private void Update() {
+        t += Time.deltaTime;
+
+        if (t >= 10) {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("AR_Obj")) {
+            List<Transform> allEnemies = FindObjectOfType<PlacementController>().allPlacedObjects;
+            allEnemies.RemoveAt(allEnemies.FindIndex(e => e.Equals(collision.transform)));
+            
+            if(allEnemies.Count == 0) {
+                FindObjectOfType<AR_DefenseController>().win();
+            }
+
+            Destroy(collision.gameObject);
+        }
+
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/PlacementController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlacementController : MonoBehaviour
+{
+    public GameObject PrefabToPlace;
+    public GameObject StartButton;
+    public List<Transform> allPlacedObjects;
+
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private ARRaycastManager arRaycastManager;
+    private GameObject lastSelectedObject;
+
+
+    private Vector2 touchPosition;
+    public bool gameStarted;
+
+    void Awake() {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+
+        StartButton.SetActive(false);
+    }
+
+    private bool TryGetTouchPosition(out Vector2 touchPosition) {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+
+        return false;
+    }
+
+    public void RemovePlanes() {
+        ARPlaneManager pm = GetComponent<ARPlaneManager>();
+        foreach (ARPlane plane in pm.trackables) {
+            plane.gameObject.SetActive(false);
+        }
+        pm.enabled = false;
+    }
+
+
+    void Update() {
+        if (gameStarted) return;
+
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+
+            touchPosition = touch.position;
+
+            if (touch.phase == TouchPhase.Began) {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hitObject;
+                if (Physics.Raycast(ray, out hitObject)) {
+                    lastSelectedObject = hitObject.transform.gameObject.CompareTag("AR_Obj") ? hitObject.transform.gameObject : null;
+
+                }
+            }
+
+            if (arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon)) {
+                Pose hitPose = hits[0].pose;
+                Vector3 pos = hitPose.position += new Vector3(0, PrefabToPlace.transform.lossyScale.y / 2, 0);
+                if (lastSelectedObject == null) {  
+                    lastSelectedObject = Instantiate(PrefabToPlace, pos, hitPose.rotation);
+                    lastSelectedObject.transform.rotation = hitPose.rotation;
+
+                    allPlacedObjects.Add(lastSelectedObject.transform);
+                    if (allPlacedObjects.Count >= 5) StartButton.SetActive(true);
+                } else {
+
+                    lastSelectedObject.transform.position = pos;
+                    lastSelectedObject.transform.rotation = hitPose.rotation;
+
+                }
+            }
+
+        }
+    }
+
+
+
+}
